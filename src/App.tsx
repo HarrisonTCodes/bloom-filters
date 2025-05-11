6ab91ddbf2fb64c066e88b75665eaaca022835d7
HarrisonTCodes <htillbackup@gmail.com>
2025-05-11 11:56:05 +0100

    feat: added initial page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import Input from './components/Input';
+
 function App() {
   return (
     <main>
-      <h1 className="text-xl">Bloom Filters</h1>
+      {/* Information */}
+      <section>
+        <h1 className="text-xl">Bloom Filters</h1>
+      </section>
+
+      {/* Visualisation */}
+      <section>
+        {/* Add to bloom filter */}
+        <section>
+          <Input />
+        </section>
+
+        {/* Bit array */}
+        <section>Bit array</section>
+
+        {/* Check if in bloom filter */}
+        <section>
+          <Input />
+        </section>
+      </section>
     </main>
   );
 }
