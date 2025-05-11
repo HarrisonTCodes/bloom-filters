01325653363b6d9f6c5031b4880e81b9771424d4
HarrisonTCodes <htillbackup@gmail.com>
2025-05-11 13:21:16 +0100

    feat: added initial bit array rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Input from './components/Input';
 
 function App() {
+  const bitCount = 64;
+
   return (
     <main>
       {/* Information */}
@@ -16,7 +18,11 @@ function App() {
         </section>
 
         {/* Bit array */}
-        <section>Bit array</section>
+        <section className="grid w-fit grid-cols-16">
+          {Array.from({ length: bitCount }).map((_, index) => (
+            <div key={`bit-${index}`} className="h-8 w-8 border border-black" />
+          ))}
+        </section>
 
         {/* Check if in bloom filter */}
         <section>
