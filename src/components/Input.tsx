243efafb20aab1dc52471b74d739c2d8eb21b5a9
HarrisonTCodes <htillbackup@gmail.com>
2025-05-11 11:55:48 +0100

    feat: added initial input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,5 @@
+export default function Input() {
+  return (
+    <input className="rounded-md border border-gray-300 bg-gray-50 p-1 text-gray-900 focus:border-blue-500 focus:ring-blue-500" />
+  );
+}
